66e9866d6997fa95486cde58745c456a56317027
Koyeo <im@koyeo.io>
2017-07-11 17:05:19 +0800

    edit route

--- Http/api.php
+++ Http/api.php
@@ -106,7 +106,7 @@ Route::group(['middleware' => 'api', 'prefix' => '/api/auth', 'namespace' => 'Mo
 
     // 获取图形验证码
     Route::get('/captcha', 'AuthController@getCaptcha')->open();
-    Route::get('/api/auth/captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha')
+    Route::get('/captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha')
         ->middleware('web')
         ->open();
 
