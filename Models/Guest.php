cfc0857bd1441d3b9a2bf15e7095a0220753e0d9
Koyeo <im@koyeo.io>
2017-09-26 17:09:17 +0800

    edit permission auth methods

--- Models/Guest.php
+++ Models/Guest.php
@@ -90,183 +90,188 @@ class Guest extends Member
                 return true;
             } else {
                 $own_scope = [];
-                foreach ($permissions as $permission_id => $permission_scope) {
-                    if ($permission_scope != '*') {
-                        $params = json_decode($permission_scope);
-                        foreach ($params as $key => $data) {
-                            if (is_array($data)) {
-                                if (is_array($own_scope[$key])) {
-                                    $own_scope[$key] = $data;
-                                } else {
-                                    array_merge($own_scope[$key], $data);
+                foreach ($permissions as $permission_id => $permission_scopes) {
+                    foreach ($permission_scopes as $permission_scope) {
+                        if ($permission_scope != '*') {
+                            $params = json_decode($permission_scope);
+                            foreach ($params as $key => $data) {
+                                if ($key == 'member_id' && $data = '$') {
+                                    $data = self::$id;
                                 }
-                            } else {
-                                if (is_array($own_scope[$key])) {
-                                    array_push($own_scope[$key], $data);
+                                if (!isset($own_scope[$key])) {
+                                    $own_scope[$key] = [];
+                                }
+                                if (is_array($data)) {
+                                    array_merge($own_scope[$key], $data);
                                 } else {
                                     $own_scope[$key][] = $data;
                                 }
+
                             }
-
                         }
-                    }
                 }
-                foreach ($scope as $key => $value) {
-                    if (!array_key_exists($key, $own_scope)) {
-                        return false;
-                    } else if (!in_array($value, $own_scope[$key])) {
-                        return false;
-                    }
-                }
-                return true;
             }
-
+            foreach ($scope as $key => $value) {
+                if (!array_key_exists($key, $own_scope)) {
+                    return false;
+                } else if (!in_array($value, $own_scope[$key])) {
+                    return false;
+                }
+            }
+            return true;
         }
 
-        return false;
     }
 
-    /**
-     * Get guest role permissions limit scope via the route.
-     *
-     * @param array $guest_permissions
-     *
-     * @return array
-     */
-    public function scope(array $guest_permissions = [])
-    {
-        $guard_fields = [];
+return false;
+}
 
-        $guest_permissions = $guest_permissions ? $guest_permissions : self::permissions();
+/**
+ * Get guest role permissions limit scope via the route.
+ *
+ * @param array $guest_permissions
+ *
+ * @return array
+ */
+public
+function scope(array $guest_permissions = [])
+{
+    $guard_fields = [];
 
-        foreach ($guest_permissions as $guest_permission) {
+    $guest_permissions = $guest_permissions ? $guest_permissions : self::permissions();
 
-            if (!$guest_permission['limit_parse']) {
+    foreach ($guest_permissions as $guest_permission) {
 
-                continue;
-            }
+        if (!$guest_permission['limit_parse']) {
 
-            $fields = json_decode($guest_permission['limit_parse'], true);
+            continue;
+        }
 
-            foreach ($fields as $filed => $value) {
+        $fields = json_decode($guest_permission['limit_parse'], true);
 
-                if (key_exists($filed, $guard_fields)) {
+        foreach ($fields as $filed => $value) {
 
-                    if (is_array($value)) {
+            if (key_exists($filed, $guard_fields)) {
 
-                        $guard_fields[$filed] = array_merge($guard_fields[$filed], $value);
+                if (is_array($value)) {
 
-                    } else {
-
-                        array_push($guard_fields[$filed], $value);
-                    }
+                    $guard_fields[$filed] = array_merge($guard_fields[$filed], $value);
 
                 } else {
 
-                    $guard_fields[$filed] = $value;
+                    array_push($guard_fields[$filed], $value);
                 }
+
+            } else {
+
+                $guard_fields[$filed] = $value;
             }
         }
-
-        foreach ($guard_fields as $field => $values) {
-
-            foreach ($values as $key => $value) {
-                switch ($value) {
-                    case '*':
-                        unset($guard_fields[$field]);
-                        break;
-                    case 'guest':
-                        $scope[$field][$key] = (string)self::id();
-                        break;
-                }
-            }
-
-            $guard_fields[$field] = array_unique($guard_fields[$field]);
-        }
-
-
-        return $guard_fields;
     }
 
-    /**
-     * Get the guest roles.
-     *
-     * @return array
-     */
-    public function roles()
-    {
-        return MemberRole::where('member_id', self::$id)->pluck('role_id')->toArray();
-    }
+    foreach ($guard_fields as $field => $values) {
 
-    /**
-     * Get the guest role permissions via route permission.
-     *
-     *
-     * @return array
-     */
-    public function permissions()
-    {
-        $permissions = [];
-
-        $role_permissions = RolePermission::whereIn('role_id', self::roles())
-            ->select('permission_id', 'permission_scope')->get()->toArray();
-
-        $member_permissions = MemberPermission::where('member_id', self::$id)
-            ->select('permission_id', 'permission_scope', 'permission_type', 'started_at', 'expired_at')
-            ->get()->toArray();
-
-        foreach ($role_permissions as $role_permission) {
-            $permissions[$role_permission['permission_id']][] = $role_permission['permission_scope'];
-        }
-
-        foreach ($member_permissions as $member_permission) {
-            if ($member_permission->permission_type == 2) {
-                if (strtotime($member_permission['started_at']) - time() > 0 && strtotime($member_permission['expired_at']) - time() < 0) {
-                    $permissions[$member_permission['permission_id']][] = $member_permission['permission_scope'];
-                }
-            }
-        }
-
-        return $permissions;
-    }
-
-
-    /**
-     * Get guest all role permissions.
-     *
-     * @return array
-     */
-    public function allPermissions()
-    {
-        return RolePermission::whereIn('role_id', self::roles())->get();
-    }
-
-    /**
-     * Add guest resource query limit condition.
-     *
-     * @param $query
-     *
-     * @return mixed
-     */
-    public function guardPermissionScope($query)
-    {
-        $scope = self::scope();
-
-        foreach ($scope as $field => $values) {
-
-            switch (count($scope[$field])) {
-                case 0:
-                    continue;
-                case 1:
-                    $query = $query->where($field, $scope[$field][0]);
+        foreach ($values as $key => $value) {
+            switch ($value) {
+                case '*':
+                    unset($guard_fields[$field]);
                     break;
-                default:
-                    $query = $query->whereIn($field, $scope[$field]);
+                case 'guest':
+                    $scope[$field][$key] = (string)self::id();
+                    break;
             }
-
         }
 
-        return $query;
+        $guard_fields[$field] = array_unique($guard_fields[$field]);
     }
+
+
+    return $guard_fields;
+}
+
+/**
+ * Get the guest roles.
+ *
+ * @return array
+ */
+public
+function roles()
+{
+    return MemberRole::where('member_id', self::$id)->pluck('role_id')->toArray();
+}
+
+/**
+ * Get the guest role permissions via route permission.
+ *
+ *
+ * @return array
+ */
+public
+function permissions()
+{
+    $permissions = [];
+
+    $role_permissions = RolePermission::whereIn('role_id', self::roles())
+        ->select('permission_id', 'permission_scope')->get()->toArray();
+
+    $member_permissions = MemberPermission::where('member_id', self::$id)
+        ->select('permission_id', 'permission_scope', 'permission_type', 'started_at', 'expired_at')
+        ->get()->toArray();
+
+    foreach ($role_permissions as $role_permission) {
+        $permissions[$role_permission['permission_id']][] = $role_permission['permission_scope'];
+    }
+
+    foreach ($member_permissions as $member_permission) {
+        if ($member_permission->permission_type == 2) {
+            if (strtotime($member_permission['started_at']) - time() > 0 && strtotime($member_permission['expired_at']) - time() < 0) {
+                $permissions[$member_permission['permission_id']][] = $member_permission['permission_scope'];
+            }
+        }
+    }
+
+    return $permissions;
+}
+
+
+/**
+ * Get guest all role permissions.
+ *
+ * @return array
+ */
+public
+function allPermissions()
+{
+    return RolePermission::whereIn('role_id', self::roles())->get();
+}
+
+/**
+ * Add guest resource query limit condition.
+ *
+ * @param $query
+ *
+ * @return mixed
+ */
+public
+function guardPermissionScope($query)
+{
+    $scope = self::scope();
+
+    foreach ($scope as $field => $values) {
+
+        switch (count($scope[$field])) {
+            case 0:
+                continue;
+            case 1:
+                $query = $query->where($field, $scope[$field][0]);
+                break;
+            default:
+                $query = $query->whereIn($field, $scope[$field]);
+        }
+
+    }
+
+    return $query;
+}
 
 }
