72f32a5c3afa596a704d4a1a292044c6fa889b7b
Koyeo <im@koyeo.io>
2017-11-03 13:00:55 +0800

    调整静态资源加载策略

--- Foundation/UrlGenerator.php
+++ Foundation/UrlGenerator.php
@@ -25,7 +25,7 @@ class UrlGenerator extends LaravelUrlGenerator
         // Once we get the root URL, we will check to see if it contains an index.php
         // file in the paths. If it does, we will remove it since it is not needed
         // for asset paths, but only for routes to endpoints in the application.
-        $root = $this->formatRoot($this->autoFormatSchemecd($path, $secure), env('ASSET_URL', env('APP_URL')));
+        $root = $this->formatRoot($this->autoFormatScheme($path, $secure), env('ASSET_URL', env('APP_URL')));
 
         return $this->removeIndex($root) . '/' . trim($path, '/');
     }
