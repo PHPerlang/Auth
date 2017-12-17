8147185096c8c8bb248b510390ecd099ad479cec
Koyeo <im@koyeo.io>
2017-12-17 10:48:03 +0800

    添加staff角色基本权限

--- Config/roles.php
+++ Config/roles.php
@@ -1,20 +1,20 @@
 <?php
 
+/**
+ * 注册角色列表
+ *
+ * 超级管理员角色拥有一切权限，不用在此注册
+ * staff 角色代表内部职工，拥有使用后台的基本权限
+ */
 return array(
-
-    'root' => [
-        'name' => '超级管理员',
-        'description' => '超级管理员，拥有系统的一切权限',
-        'permissions' => [
-            'get@api/auth/role'
-        ],
-    ],
 
     'staff' => [
         'name' => '职员',
         'description' => '内部职员角色，拥有基本的后台登录权限',
         'permissions' => [
-            'get@admin'
+            'get@admin/auth/member/profile',
+            'get@admin/auth/member/setting',
+            'get@admin/auth/member/login/log',
         ]
     ]
 
